c57c2ab263b7f3a54a1b9a5912b78bcdbe6022c6
Maulidani <maulidanimahmud@gmail.com>
2022-01-20 09:09:06 +0800

    Initial commit

---
+++ app/src/main/java/com/skripsi/ambulanapp/customer/MainCustomerActivity.kt
@@ -0,0 +1,12 @@
+package com.skripsi.ambulanapp.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.skripsi.ambulanapp.R
+
+class MainCustomerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_customer)
+    }
+}
---
+++ app/src/main/java/com/skripsi/ambulanapp/driver/MainDriverActivity.kt
@@ -0,0 +1,12 @@
+package com.skripsi.ambulanapp.driver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.skripsi.ambulanapp.R
+
+class MainDriverActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_driver)
+    }
+}
