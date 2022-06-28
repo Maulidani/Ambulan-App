f1a86f7d1b07164c955c41900400895d4e02bf4b
maulidani <maulidanimahmud@gmail.com>
2022-06-28 18:59:46 +0800

    update

--- app/src/main/java/com/skripsi/ambulanapp/network/Link.kt
+++ app/src/main/java/com/skripsi/ambulanapp/network/Link.kt
@@ -2,7 +2,8 @@ package com.skripsi.ambulanapp.network
 
 class Link {
     companion object {
-        const val BASE_URL = "http://192.168.210.5:8000/"
+//        const val BASE_URL = "http://192.168.210.5:8000/"
+        const val BASE_URL = "https://skripsi-andi-jul.000webhostapp.com/ambulan-app/public/"
         const val URL_IMAGE_USER = BASE_URL +"image/user/"
         const val URL_IMAGE_HOSPITAL = BASE_URL +"image/hospital/"
     }
